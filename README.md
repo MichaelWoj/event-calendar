9e1046db63a6a5449232b1776aa500946430db48
MichaelWoj <wojnowskimichael@gmail.com>
2024-09-30 19:00:51 +0200

    Code is read

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calendar-event-app
+
+Here is how to run the project
+
+1. Create a file name ".env" (without the quotes) in calendar_event_app. In that file write "API_KEY = your_api_key"
+2. While in the "calendar_event_project" run the "python manage.py fetch_events" command to get the events from the API 
+3. Run the application with the "python manage.py runserver" command.
--- calendar_event_project/calendar_event_app/management/commands/fetch_events.py
+++ calendar_event_project/calendar_event_app/management/commands/fetch_events.py
@@ -2,7 +2,6 @@ import requests
 from django.core.management.base import BaseCommand
 from calendar_event_app.models import Events, Tag
 from decouple import config
-from django.views.decorators.debug import sensitive_variables
 
 API_URL = "https://rekrutacja.teamwsuws.pl/events"
 DETAIL_API_URL = "https://rekrutacja.teamwsuws.pl/events/{id}"
@@ -16,7 +15,7 @@ class Command(BaseCommand):
     help = 'Fetch data from the external API and populate the database'
 
     def handle(self, *args, **kwargs):
-        # First API call to get the list of events
+
         response = requests.get(API_URL, headers=headers)
         if response.status_code == 200:
             events_data = response.json()
@@ -24,24 +23,22 @@ class Command(BaseCommand):
             for event_data in events_data:
                 event_id = event_data['id']
         
-                # Make the second API call to get detailed information for each event
                 # If the API was a paid service per call I would do this differently.
                 detailed_response = requests.get(DETAIL_API_URL.format(id=event_id), headers=headers)
                 if detailed_response.status_code == 200:
                     detailed_event_data = detailed_response.json()
 
-                    # Create or update the Event instance
                     event, created = Events.objects.update_or_create(
-                        id=event_id,  # Use the ID from the API
+                        id=event_id,  
                         defaults={
                             'name': event_data['name'],
                             'start_time': event_data['start_time'],
                             'duration': event_data['duration'],
                             'image_url': event_data['image_url'],
                             'short_description': event_data['short_description'],
-                            'location': detailed_event_data.get('location', ''),  # From second API
-                            'registration_link': detailed_event_data.get('registration_link', ''),  # From second API
-                            'long_description': detailed_event_data.get('long_description', ''),  # From second API
+                            'location': detailed_event_data.get('location', ''),  
+                            'registration_link': detailed_event_data.get('registration_link', ''), 
+                            'long_description': detailed_event_data.get('long_description', ''), 
                         }
                     )
                     if created:
@@ -49,14 +46,14 @@ class Command(BaseCommand):
                     else:
                         self.stdout.write(self.style.SUCCESS(f"Updated event: {event.name}"))
 
-                    # Now that the event is saved, handle the tags
-                    event.tags.clear()  # Clear existing tags
+
+                    event.tags.clear() 
                     for tag_data in event_data.get('tags', []):
                         tag_name = tag_data['name']
                         tag, _ = Tag.objects.get_or_create(name=tag_name)
                         event.tags.add(tag)
 
-                    event.save()  # Save the event after adding tags
+                    event.save()
 
             self.stdout.write(self.style.SUCCESS('Successfully populated the database'))
         else:
